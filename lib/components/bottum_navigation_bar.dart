7cd6a2810ae6833d4e7bbc3e75c99208dde80963
Menuz <menuzhewage@gmail.com>
2025-02-07 19:21:41 +0530

    update login and register page

--- lib/components/bottum_navigation_bar.dart
+++ lib/components/bottum_navigation_bar.dart
@@ -1,8 +1,8 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavigationBarExample extends StatelessWidget {
-  const BottomNavigationBarExample({super.key});
+class BottomNavigationBar extends StatelessWidget {
+  const BottomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mahaweli_admin_system/components/bottum_navigation_bar.dart';
+import 'package:mahaweli_admin_system/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: BottomNavigationBarExample(),
+      home: Login(),
     );
   }
 }
